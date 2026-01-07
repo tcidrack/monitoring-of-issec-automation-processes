3ece76aefbdaabf8d57116fc0c31f73641fc3aa6
Thalison Cidrack <thalison.silva@mv.com.br>
2026-01-07 14:46:43 -0300

    use env API url for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function App() {
   const [processos, setProcessos] = useState([]);
